043bec8751e73a27f42dfcbc5aa8e5ff65f476a7
Administrator <731511202@qq.com>
2018-07-17 17:54:24 +0800

    swagger2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.demo.common.MessageCode;
 import com.example.demo.model.base.BaseResponse;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +20,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value="添加用户基础信息", notes="根据User对象创建用户。")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value="/add", method = RequestMethod.POST)
     public BaseResponse addUser(@RequestBody @NonNull User user){
         BaseResponse baseResponse = new BaseResponse(MessageCode.SUCCESS, MessageCode.SUCCESS_MSG);
--- src/main/java/com/example/demo/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/OrderServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.demo.mapper.OrderMapper;
 import com.example.demo.model.Order;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service(value="orderService")
 public class OrderServiceImpl implements OrderService {
 
     @Autowired
