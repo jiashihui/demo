0512859657cd5582cb74be94ed3f887777964b7b
Administrator <731511202@qq.com>
2018-08-07 18:23:43 +0800

    事务

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,18 +28,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Transactional//注解式事务
     @ApiOperation(value="添加用户基础信息", notes="根据User对象创建用户。")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value="/add", method = RequestMethod.POST)
     public BaseResponse addUser(@RequestBody @NonNull User user){
         BaseResponse baseResponse = new BaseResponse(MessageCode.SUCCESS, MessageCode.SUCCESS_MSG);
-
-        int result = transactionTemplate.execute((TransactionStatus transactionStatus) -> {
-            int r = userService.insert(user);
-            int a = 5/0;
-            return r;
-        });
-
+        int result = userService.insert(user);
+        int a = 5/0;
+//        int result = transactionTemplate.execute((TransactionStatus transactionStatus) -> {
+//            int r = userService.insert(user);
+//            int a = 5/0;
+//            return r;
+//        });
         if(result != 1){
             baseResponse.setCode(MessageCode.FAIL);
             baseResponse.setMessage(MessageCode.FAIL_MSG);
